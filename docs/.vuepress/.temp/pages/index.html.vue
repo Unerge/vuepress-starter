b5e6ff94389be4311de2a13d5ee4d21935e5841c
Larch4 <114554982+Larch4@users.noreply.github.com>
2024-09-24 22:58:09 +0800

    New

--- docs/.vuepress/.temp/pages/index.html.vue
+++
@@ -1,4 +0,0 @@
-<template><div><p>'# Hello VuePress'</p>
-</div></template>
-
-
--- docs/.vuepress/config.js
+++
@@ -1,8 +0,0 @@
-import { viteBundler } from '@vuepress/bundler-vite'
-import { defaultTheme } from '@vuepress/theme-default'
-import { defineUserConfig } from 'vuepress'
-
-export default defineUserConfig({
-  bundler: viteBundler(),
-  theme: defaultTheme(),
-})
