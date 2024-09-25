e50874a5d09a1e100bad2fc8b936b25dc54d6f4d
Larch4 <114554982+Larch4@users.noreply.github.com>
2024-09-25 11:38:59 +0800

    New

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,14 +1,1 @@
 export const themeData = JSON.parse("{\"repo\":\"Unerge/vuepress-starter\",\"docsDir\":\"docs\",\"docsBranch\":\"docs\",\"editLinks\":true,\"editLinkText\":\"在 GitHub 上编辑此页\",\"navbar\":[{\"text\":\"beta\",\"link\":\"/beta/\"},{\"text\":\"GPTstort\",\"link\":\"/GPTstory/\"},{\"text\":\"tech\",\"link\":\"/tech/\"},{\"text\":\"房屋建筑学\",\"link\":\"/房屋建筑学/\"},{\"text\":\"工程力学\",\"link\":\"/工程力学/\"},{\"text\":\"管理学\",\"link\":\"/管理学/\"},{\"text\":\"近代史\",\"link\":\"/近代史/\"},{\"text\":\"经济学\",\"link\":\"/经济学/\"},{\"text\":\"英语\",\"link\":\"/英语/\"}],\"sidebar\":{\"/beta/\":[{\"text\":\"beta\",\"collapsible\":true,\"children\":[\"/beta/\"]}],\"/GPTstory/\":[{\"text\":\"GPTstory\",\"collapsible\":true,\"children\":[\"1.md\",\"2.md\"]}],\"/tech/\":[{\"text\":\"tech\",\"collapsible\":true,\"children\":[\"1.md\",\"2.md\",\"3.md\",\"4.md\",\"5.md\",\"6.md\",\"7.md\",\"8.md\"]}],\"/工程力学/\":[{\"text\":\"工程力学\",\"collapsible\":true,\"children\":[\"复习资料1.md\",\"题目1的解答.md\",\"题目解答2.md\"]}],\"/房屋建筑学/\":[{\"text\":\"房屋建筑学\",\"collapsible\":true,\"children\":[\"复习资料 01.md\"]}],\"/管理学/\":[{\"text\":\"管理学\",\"collapsible\":true,\"children\":[\"第一章.md\"]}],\"/经济学/\":[{\"text\":\"经济学\",\"collapsible\":true,\"children\":[\"经济学期末考-复习1.md\",\"经济学期末考-复习2.md\"]}],\"/英语/\":[{\"text\":\"英语\",\"collapsible\":true,\"children\":[\"期末资料 .md\"]}],\"/近代史/\":[{\"text\":\"近代史\",\"collapsible\":true,\"children\":[\"期末资料 .md\"]}]},\"logo\":\"/img/logo1251.jpg\",\"search\":true,\"searchMaxSuggestions\":10,\"colorMode\":\"light\",\"colorModeSwitch\":true,\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateThemeData) {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ themeData }) => {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  })
-}
--- docs/.vuepress/.temp/pages/近代史/期末资料 .html.js
+++ docs/.vuepress/.temp/pages/近代史/期末资料 .html.js
@@ -1,16 +1,3 @@
 import comp from "C:/Users/27493/Documents/GitHub/vuepress-starter/docs/.vuepress/.temp/pages/近代史/期末资料 .html.vue"
 const data = JSON.parse("{\"path\":\"/%E8%BF%91%E4%BB%A3%E5%8F%B2/%E6%9C%9F%E6%9C%AB%E8%B5%84%E6%96%99%20.html\",\"title\":\"第八~十章 “现代史”专题训练\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"一. 单选题（共46题，50.5分）\",\"slug\":\"一-单选题-共46题-50-5分\",\"link\":\"#一-单选题-共46题-50-5分\",\"children\":[]},{\"level\":3,\"title\":\"二. 多选题（共25题，27.5分）\",\"slug\":\"二-多选题-共25题-27-5分\",\"link\":\"#二-多选题-共25题-27-5分\",\"children\":[]},{\"level\":3,\"title\":\"三. 判断题（共20题，22分）\",\"slug\":\"三-判断题-共20题-22分\",\"link\":\"#三-判断题-共20题-22分\",\"children\":[]},{\"level\":3,\"title\":\"一. 单选题（共100题，65分）\",\"slug\":\"一-单选题-共100题-65分\",\"link\":\"#一-单选题-共100题-65分\",\"children\":[]},{\"level\":3,\"title\":\"二. 多选题（共50题，35分）\",\"slug\":\"二-多选题-共50题-35分\",\"link\":\"#二-多选题-共50题-35分\",\"children\":[]}],\"git\":{\"updatedTime\":1727188073000,\"contributors\":[{\"name\":\"Larch4\",\"email\":\"114554982+Larch4@users.noreply.github.com\",\"commits\":1}]},\"filePathRelative\":\"近代史/期末资料 .md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
