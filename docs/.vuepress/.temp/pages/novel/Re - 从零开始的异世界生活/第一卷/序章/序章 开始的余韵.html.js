c89a1bb578ef7ac27f3098f50a15485e78f1ac57
Larch4 <74816859+IGCrystal@users.noreply.github.com>
2024-10-11 11:46:50 +0800

    New Novel

--- docs/.vuepress/.temp/pages/novel/Re - 从零开始的异世界生活/第一卷/序章/序章 开始的余韵.html.js
+++ docs/.vuepress/.temp/pages/novel/Re - 从零开始的异世界生活/第一卷/序章/序章 开始的余韵.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/27493/Documents/GitHub/vuepress-starter/docs/.vuepress/.temp/pages/novel/Re - 从零开始的异世界生活/第一卷/序章/序章 开始的余韵.html.vue"
-const data = JSON.parse("{\"path\":\"/novel/Re%20-%20%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E7%9A%84%E5%BC%82%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB/%E7%AC%AC%E4%B8%80%E5%8D%B7/%E5%BA%8F%E7%AB%A0/%E5%BA%8F%E7%AB%A0%20%E5%BC%80%E5%A7%8B%E7%9A%84%E4%BD%99%E9%9F%B5.html\",\"title\":\"序章 开始的余韵\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"序章 开始的余韵\"},\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"novel/Re - 从零开始的异世界生活/第一卷/序章/序章 开始的余韵.md\"}")
+const data = JSON.parse("{\"path\":\"/novel/Re%20-%20%E4%BB%8E%E9%9B%B6%E5%BC%80%E5%A7%8B%E7%9A%84%E5%BC%82%E4%B8%96%E7%95%8C%E7%94%9F%E6%B4%BB/%E7%AC%AC%E4%B8%80%E5%8D%B7/%E5%BA%8F%E7%AB%A0/%E5%BA%8F%E7%AB%A0%20%E5%BC%80%E5%A7%8B%E7%9A%84%E4%BD%99%E9%9F%B5.html\",\"title\":\"序章 开始的余韵\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"序章 开始的余韵\"},\"headers\":[],\"git\":{\"updatedTime\":1728559441000,\"contributors\":[{\"name\":\"Larch4\",\"email\":\"74816859+IGCrystal@users.noreply.github.com\",\"commits\":1}]},\"filePathRelative\":\"novel/Re - 从零开始的异世界生活/第一卷/序章/序章 开始的余韵.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/novel/index.html.js
+++ docs/.vuepress/.temp/pages/novel/index.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/27493/Documents/GitHub/vuepress-starter/docs/.vuepress/.temp/pages/novel/index.html.vue"
-const data = JSON.parse("{\"path\":\"/novel/\",\"title\":\"小说\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"小说\",\"heroImage\":\"logo.jpg\",\"heroText\":\"Novel\",\"tagline\":\"小说\",\"footer\":\"冰苷晶 © 2024\"},\"headers\":[],\"git\":{\"updatedTime\":1728530423000,\"contributors\":[{\"name\":\"Larch4\",\"email\":\"74816859+IGCrystal@users.noreply.github.com\",\"commits\":1}]},\"filePathRelative\":\"novel/README.md\"}")
+const data = JSON.parse("{\"path\":\"/novel/\",\"title\":\"小说\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"小说\",\"heroImage\":\"logo.jpg\",\"heroText\":\"Novel\",\"tagline\":\"小说\",\"footer\":\"冰苷晶 © 2024\"},\"headers\":[],\"git\":{\"updatedTime\":1728559441000,\"contributors\":[{\"name\":\"Larch4\",\"email\":\"74816859+IGCrystal@users.noreply.github.com\",\"commits\":2}]},\"filePathRelative\":\"novel/README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
