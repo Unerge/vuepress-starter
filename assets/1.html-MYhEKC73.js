323d540d2de29b66f1f4a2e382d481aeff6f547e
IGCrystal <74816859+IGCrystal@users.noreply.github.com>
2024-11-01 09:14:57 +0000

    Deploying to main from @ Unerge/vuepress-starter@a5c1aae9f493b6e54ae160dbe985374be7099f30 🚀

---
+++ assets/1.html-MYhEKC73.js
@@ -0,0 +1,35 @@
+import{_ as s,c as e,a,o as i}from"./app-DQhclFMQ.js";const l={};function r(t,n){return i(),e("div",null,n[0]||(n[0]=[a(`<h1 id="_2024-10-9" tabindex="-1"><a class="header-anchor" href="#_2024-10-9"><span>2024/10/9</span></a></h1><h2 id="words" tabindex="-1"><a class="header-anchor" href="#words"><span>Words</span></a></h2><div class="language-word line-numbers-mode" data-highlighter="prismjs" data-ext="word" data-title="word"><pre><code><span class="line"> admit</span>
+<span class="line"> argue</span>
+<span class="line"> remark</span>
+<span class="line"> fall out of</span>
+<span class="line"> increase</span>
+<span class="line"> expense</span>
+<span class="line"> concentrate</span>
+<span class="line"> interruption</span>
+<span class="line"> chemist</span>
+<span class="line"> brush aside</span>
+<span class="line"> put to the test</span>
+<span class="line"> brought under discussion</span>
+<span class="line"> taken into accout</span>
+<span class="line"> retire</span>
+<span class="line"> recover</span>
+<span class="line"> attempt</span>
+<span class="line"> pause</span>
+<span class="line"> promiss</span>
+<span class="line"> eventually</span>
+<span class="line"> casually</span>
+<span class="line"> secretly</span>
+<span class="line"> assess</span>
+<span class="line"> form</span>
+<span class="line"> prove</span>
+<span class="line"> slight</span>
+<span class="line"> curiosity</span>
+<span class="line"> disappointment</span>
+<span class="line"> persuade</span>
+<span class="line"> refuse</span>
+<span class="line"> secure</span>
+<span class="line"> repeat</span>
+<span class="line"> recite</span>
+<span class="line"> duty</span>
+<span class="line"> greet</span>
+<span class="line"></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="sentences" tabindex="-1"><a class="header-anchor" href="#sentences"><span>Sentences</span></a></h2><h3 id="活动类作文" tabindex="-1"><a class="header-anchor" href="#活动类作文"><span>活动类作文</span></a></h3><ul><li><p>From this experience, not only have I learnt that knowledge comes alive when put into practice, but it has also contributed to my personal growth and success.</p></li><li><p>All in all, in my English learning proess, I firmly belive that &quot;Where there is a will, there is a way&quot;. The man who has made up his mind to win never say impossible. And learning English is the key to unlocking the treasures of knowledge.</p></li><li><p>As the saying goes, a journey of a thousand miles begins with a single step. With motivation and determination, anyone can succeed in learning English.</p></li><li><p>From the moment I started the course, I fell in love with it. Not only did it provide me with a chance to experience and appreciate the beauty of calligraphy, but it also served as a window for me to explore the rich traditional Chinese culture.</p></li></ul>`,6)]))}const d=s(l,[["render",r],["__file","1.html.vue"]]),p=JSON.parse('{"path":"/learn/%E8%8B%B1%E8%AF%AD/CET-4/1.html","title":"2024/10/9","lang":"en-US","frontmatter":{},"headers":[{"level":2,"title":"Words","slug":"words","link":"#words","children":[]},{"level":2,"title":"Sentences","slug":"sentences","link":"#sentences","children":[{"level":3,"title":"活动类作文","slug":"活动类作文","link":"#活动类作文","children":[]}]}],"git":{"updatedTime":1729338822000,"contributors":[{"name":"冰苷晶","email":"74816859+IGCrystal@users.noreply.github.com","commits":1}]},"filePathRelative":"learn/英语/CET-4/1.md"}');export{d as comp,p as data};
