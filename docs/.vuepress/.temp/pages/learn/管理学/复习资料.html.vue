d9dc6c5bcf81db126fd07c6dc12e56a9c0efd2ed
冰苷晶 <74816859+IGCrystal@users.noreply.github.com>
2024-11-02 21:19:34 +0800

    New

--- docs/.vuepress/.temp/pages/learn/管理学/复习资料.html.vue
+++ docs/.vuepress/.temp/pages/learn/管理学/复习资料.html.vue
@@ -14,8 +14,8 @@
 <p>该复习资料涵盖全书, 但仍然有遗漏之处. 具体内容以书本为准.</p>
 </div>
 <h2 id="中国古代思想" tabindex="-1"><a class="header-anchor" href="#中国古代思想"><span>中国古代思想</span></a></h2>
-<details>
-<summary>点击展开</summary>
+<details id="my-details">
+<summary>展开/收缩</summary>
 <ul>
 <li>顺道无为思想</li>
 <li>重人求和思想</li>
@@ -24,35 +24,204 @@
 </ul>
 </details>
 <h2 id="西方工厂制度早期的管理思想" tabindex="-1"><a class="header-anchor" href="#西方工厂制度早期的管理思想"><span>西方工厂制度早期的管理思想</span></a></h2>
-<details>
-<summary>点击展开</summary>
+<details id="my-details">
+<summary>展开/收缩</summary>
 <ol>
 <li><strong>斯密认为, 劳动是国民财富的重要源泉之一</strong>.</li>
-<li>一国财富的多寡, 取决于两个因素:
+<li><strong>1856</strong> 年出生的 <strong>泰勒</strong> 被后人称为 &quot; <strong>科学管理之父</strong> &quot;.</li>
+</ol>
+</details>
+<h3 id="一国财富的多寡-取决于两个因素" tabindex="-1"><a class="header-anchor" href="#一国财富的多寡-取决于两个因素"><span>一国财富的多寡, 取决于两个因素:</span></a></h3>
+<details id="my-details">
+<summary>展开/收缩</summary>
 <ul>
 <li>一是该国从事有用劳动的居民在总人口所占的比重;</li>
 <li>二是这些人的劳动熟练程度、劳动技巧和判断力的高低. 财富的增加可以提高人民的幸福程度, 而提高劳动者的技巧的熟练程度, 从而提高劳动生产率, 则是增加一国物质财富的重要途径. 劳动分工有助于这个目标的实现.</li>
 </ul>
-</li>
-<li>为什么&quot;同样数量的劳动者因为有了劳动分工就能完成更多量的工作呢&quot;? 斯密认为, 原因有三个:
+</details>
+<h3 id="为什么-同样数量的劳动者因为有了劳动分工就能完成更多量的工作呢-斯密认为-原因有三个" tabindex="-1"><a class="header-anchor" href="#为什么-同样数量的劳动者因为有了劳动分工就能完成更多量的工作呢-斯密认为-原因有三个"><span>为什么&quot;同样数量的劳动者因为有了劳动分工就能完成更多量的工作呢&quot;? 斯密认为, 原因有三个:</span></a></h3>
+<details id="my-details">
+<summary>展开/收缩</summary>
 <ul>
 <li>第一, 劳动分工可以使工人重复完成简单的操作, 从而可以提高劳动熟练程度, 提高劳动效率;</li>
 <li>第二, 分工节省了通常由一种工作转到其他工作所损失的时间;</li>
 <li>第三, 分工使劳动简化, 使工具专门化, 从而有利于创作新工具和改进设备. 而新机械的发明和利用,又使得劳动进一步简化和减少, 从而使一个人能够完成许多人的工作.</li>
 </ul>
-</li>
-<li><strong>1856</strong> 年出生的 <strong>泰勒</strong> 被后人称为 &quot; <strong>科学管理之父</strong> &quot;.</li>
-</ol>
 </details>
 <h2 id="学习管理学的意义" tabindex="-1"><a class="header-anchor" href="#学习管理学的意义"><span>学习管理学的意义</span></a></h2>
-<details>
-<summary>点击展开</summary>
+<details id="my-details">
+<summary>展开/收缩</summary>
 <ol>
-<li>学习管理学, 首先可以让我们了解管理的一般规律.</li>
-<li>学习管理学, 不仅可以帮助我们形成理性分析能力, 而且可以提升我们以直觉判断为基础的决策或决断能力.</li>
-<li>学习管理学, 归根结底, 是要指导我们的管理实践, 是要提升我们的管理水平.</li>
+<li>
+<p>学习管理学, 首先可以让我们了解管理的一般规律.</p>
+</li>
+<li>
+<p>学习管理学, 不仅可以帮助我们形成理性分析能力, 而且可以提升我们以直觉判断为基础的决策或决断能力.</p>
+</li>
+<li>
+<p>学习管理学, 归根结底, 是要指导我们的管理实践, 是要提升我们的管理水平.</p>
+</li>
 </ol>
 </details>
-</div></template>
+<h2 id="组织的特征" tabindex="-1"><a class="header-anchor" href="#组织的特征"><span>组织的特征</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<p>   <strong>第一</strong> , 组织是由两个或两个以上的成员构成的.</p>
+<p>   <strong>第二</strong> , 组织具有明确的目标.</p>
+<p>   <strong>第三</strong> , 组织具有特殊的活动.</p>
+<p>   <strong>第四</strong> , 任何组织在一定程度上都是独立的存在, 因此与外部社会有着相对明确的界限.</p>
+</details>
+<h2 id="管理工作内容" tabindex="-1"><a class="header-anchor" href="#管理工作内容"><span>管理工作内容</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<p>   <strong>决策  组织  领导  控制  创新</strong></p>
+<p>   <strong>决策</strong> 是组织在未来众多的行动可能中选择一个比较合理的方案。为选择正确的行动方向、确定合理的行动目标，管理者首先要研究组织活动的 <strong>内外部背景</strong> 。</p>
+</details>
+<h2 id="系统原理" tabindex="-1"><a class="header-anchor" href="#系统原理"><span>系统原理</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<p>  系统可以分为不同类型, 从系统形成方式来看, 可分为 <strong>自然系统</strong> 与 <strong>人造系统</strong>.</p>
+<ul>
+<li>整体性</li>
+<li>相关性</li>
+<li>有序性</li>
+<li>与外部的互动性</li>
+</ul>
+</details>
+<h2 id="管理的基本方法" tabindex="-1"><a class="header-anchor" href="#管理的基本方法"><span>管理的基本方法</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ul>
+<li>理性分析</li>
+<li>直觉判断</li>
+</ul>
+</details>
+<h2 id="管理要素" tabindex="-1"><a class="header-anchor" href="#管理要素"><span>管理要素</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ol>
+<li>计划</li>
+<li>组织</li>
+<li>指挥</li>
+<li>协调</li>
+<li>控制</li>
+</ol>
+</details>
+<h2 id="法理性权力" tabindex="-1"><a class="header-anchor" href="#法理性权力"><span>法理性权力</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ol>
+<li>
+<p>为管理的连续性奠定了基础。因为权力是赋予职务而不是个人的，因此权力的运用不会因领导人的更换而中断。</p>
+</li>
+<li>
+<p>合理性。担任职务的人员是按照完成任务所需的能力来挑选的。</p>
+</li>
+<li>
+<p>领导人可以借助法律手段来保证权力的行使。</p>
+</li>
+<li>
+<p>所有权力都有明确的规定，而且是按照组织所需的职能加以详细划分的。</p>
+</li>
+</ol>
+</details>
+<h2 id="协作系统的三个基本要素" tabindex="-1"><a class="header-anchor" href="#协作系统的三个基本要素"><span>协作系统的三个基本要素</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ol>
+<li>协作的意愿</li>
+<li>共同的目标</li>
+<li>信息的沟通</li>
+</ol>
+</details>
+<h2 id="决策过程" tabindex="-1"><a class="header-anchor" href="#决策过程"><span>决策过程</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<h3 id="管理的实质是决策" tabindex="-1"><a class="header-anchor" href="#管理的实质是决策"><span>管理的实质是决策.</span></a></h3>
+<ol>
+<li>情报活动</li>
+<li>设计活动</li>
+<li>抉择活动</li>
+<li>审查活动</li>
+</ol>
+</details>
+<h2 id="决策的准则" tabindex="-1"><a class="header-anchor" href="#决策的准则"><span>决策的准则</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<table>
+<thead>
+<tr>
+<th style="text-align:left">1</th>
+<th style="text-align:left">2</th>
+<th style="text-align:left">3</th>
+<th style="text-align:left">4</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td style="text-align:left">情报活动</td>
+<td style="text-align:left">设计活动</td>
+<td style="text-align:left">抉择活动</td>
+<td style="text-align:left">审查活动</td>
+</tr>
+</tbody>
+</table>
+<blockquote>
+<p>西蒙认为，人们习惯上运用 &quot;最优&quot; 或 &quot;绝对的理性&quot; 作为决策的准则。根据这个准则进行决策需要三个前提:</p>
+</blockquote>
+<p>   <strong>一</strong> 是决策者对所有可供选择的方案及其执行结果无所不知。</p>
+<p>   <strong>二</strong> 是决策者具有无限的估算能力;</p>
+<p>   <strong>三</strong> 是决策者的脑中对各种可能的结果有一个 &quot;完全而一贯的优先顺序&quot; 。</p>
+</details>
+<h2 id="管理本质的协调研究" tabindex="-1"><a class="header-anchor" href="#管理本质的协调研究"><span>管理本质的协调研究</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<blockquote>
+<p>明茨伯格指出: 无论企业采用的具体协调方法有多少种, 都可以归纳为六种基本的机制</p>
+</blockquote>
+<ol>
+<li>相互调适</li>
+<li>直接监督</li>
+<li>工作程序标准化</li>
+<li>成果标准化或产出标准化</li>
+<li>技术(技能)以及知识标准化</li>
+<li>规范标准化</li>
+</ol>
+</details>
+<h2 id="组织趋同的现象观察" tabindex="-1"><a class="header-anchor" href="#组织趋同的现象观察"><span>组织趋同的现象观察</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<p><strong>制度化</strong> 是指外部的制度环境(价值判断、文化观念、社会期待等)渗入组织内部并影响内部组织行为的过程.</p>
+</details>
+<h2 id="合法性导致组织趋同的三个不同机制" tabindex="-1"><a class="header-anchor" href="#合法性导致组织趋同的三个不同机制"><span>合法性导致组织趋同的三个不同机制:</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ol>
+<li>强迫性</li>
+<li>魔方机制</li>
+<li>社会规范机制</li>
+</ol>
+</details>
+<h2 id="群体决策的优点" tabindex="-1"><a class="header-anchor" href="#群体决策的优点"><span>群体决策的优点</span></a></h2>
+<details id="my-details">
+<summary>展开/收缩</summary>
+<ol>
+<li>有利于集中不同领域专家的智慧, 应付日益复杂的决策问题. 通过这些专家的广泛参与,专家可以对决策问题提出建设性意见, 有利于在决策方案得以贯彻实施之前, 发现存在的问题, 提高决策的针对性.</li>
+<li>能够利用更多的知识优势, 借助更多的信息, 形成更多的可行性方案.</li>
+<li>具有不同背景、经验的不同</li>
+</ol>
+</details></div></template>
 
 
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  // 获取页面中所有的 <details> 元素
+  const detailsElements = document.querySelectorAll("details");
+  // 为每个 <details> 元素添加 open 属性
+  detailsElements.forEach((details) => {
+    details.setAttribute("open", true);
+  });
+});
+</script>
