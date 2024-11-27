4b6219c7262d76359372479789f47637e8300917
冰苷晶 <2749332490@qq.com>
2024-11-27 13:06:16 +0800

    Changes

--- docs/.vuepress/.temp/pages/tech/11.html.js
+++ docs/.vuepress/.temp/pages/tech/11.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/27493/Documents/GitHub/vuepress-starter/docs/.vuepress/.temp/pages/tech/11.html.vue"
-const data = JSON.parse("{\"path\":\"/tech/11.html\",\"title\":\"创新创业作业\",\"lang\":\"zh-CN\",\"frontmatter\":{\"tags\":[\"技术文档\"],\"prev\":{\"link\":\"./10.md\",\"text\":\"在宝塔面板中 Docker 环境安装 Tailchat\"},\"next\":false,\"title\":\"创新创业作业\"},\"headers\":[],\"readingTime\":{\"minutes\":0.53,\"words\":159},\"filePathRelative\":\"tech/11.md\",\"categoryList\":[{\"id\":\"d9f913\",\"sort\":10004,\"name\":\"tech\"}],\"bulletin\":false}")
+const data = JSON.parse("{\"path\":\"/tech/11.html\",\"title\":\"创新创业作业\",\"lang\":\"zh-CN\",\"frontmatter\":{\"tags\":[\"技术文档\"],\"prev\":{\"link\":\"./10.md\",\"text\":\"在宝塔面板中 Docker 环境安装 Tailchat\"},\"next\":false,\"title\":\"创新创业作业\"},\"headers\":[],\"readingTime\":{\"minutes\":0.69,\"words\":206},\"filePathRelative\":\"tech/11.md\",\"categoryList\":[{\"id\":\"d9f913\",\"sort\":10004,\"name\":\"tech\"}],\"bulletin\":false}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
