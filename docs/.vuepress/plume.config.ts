2b3ba4722b574ce6c68a6e65b4f18ece292aec93
冰苷晶 <2749332490@qq.com>
2024-12-16 19:54:24 +0800

    🐞 fix: change footer

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -3,7 +3,7 @@ import navbar from './navbar';
 
 export default defineThemeConfig({
     logo: '/logo.png',
-    docsRepo: 'https://github.com/Unerge/vuepress-starter',
+    docsRepo: 'https://github.com/WentUrc/vuepress-starter',
     docsBranch:'docs',
     docsDir: 'docs',
 
@@ -15,8 +15,32 @@ export default defineThemeConfig({
 
     navbar,
 
-    footer: {
-        message: 'Ice Glycoside Crystal',
-        copyright: '冰苷晶 © 2024',
-    },
+footer: {
+    message: `
+        <span style="
+            font-size: 19.5px;
+        ">
+            == Ice Glycoside Crystal ==
+        </span>
+    `,
+    copyright: `
+        <div style="
+            display: flex; 
+            align-items: center; 
+            justify-content: center;
+            gap: 8px;">
+            <a aria-label="Homepage" title="WentUrc Documents" href="https://docs.wenturc.com">
+                <img src="logo.svg" width="20" height="20" />
+            </a>
+            <span>© 2024 - present
+                <span style="
+                    background: linear-gradient(to right, #4facfe, #00f2fe);
+                    -webkit-background-clip: text;
+                    -webkit-text-fill-color: transparent;">
+                        冰苷晶 Cabinina
+                </span>
+            </span>
+        </div>
+    `,
+},
 });
