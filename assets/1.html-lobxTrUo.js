e7d04d734bfe1bb31c767d787eb8261f51b94811
IGCrystal <74816859+IGCrystal@users.noreply.github.com>
2024-12-16 12:54:07 +0000

    Deploying to main from @ WentUrc/vuepress-starter@d65934a12bb455b523a5ab6e84f72d68e0a1692e 🚀

---
+++ assets/1.html-lobxTrUo.js
@@ -0,0 +1,34 @@
+import{_ as s,c as e,a,o as i}from"./app-CX8pk1SR.js";const l={};function t(p,n){return i(),e("div",null,n[0]||(n[0]=[a(`<h2 id="words" tabindex="-1"><a class="header-anchor" href="#words"><span>Words</span></a></h2><div class="language-word line-numbers-mode" data-ext="word" data-title="word"><button class="copy" title="复制代码" data-copied="已复制"></button><pre class="shiki shiki-themes vitesse-light vitesse-dark vp-code"><code><span class="line"><span> admit</span></span>
+<span class="line"><span> argue</span></span>
+<span class="line"><span> remark</span></span>
+<span class="line"><span> fall out of</span></span>
+<span class="line"><span> increase</span></span>
+<span class="line"><span> expense</span></span>
+<span class="line"><span> concentrate</span></span>
+<span class="line"><span> interruption</span></span>
+<span class="line"><span> chemist</span></span>
+<span class="line"><span> brush aside</span></span>
+<span class="line"><span> put to the test</span></span>
+<span class="line"><span> brought under discussion</span></span>
+<span class="line"><span> taken into accout</span></span>
+<span class="line"><span> retire</span></span>
+<span class="line"><span> recover</span></span>
+<span class="line"><span> attempt</span></span>
+<span class="line"><span> pause</span></span>
+<span class="line"><span> promiss</span></span>
+<span class="line"><span> eventually</span></span>
+<span class="line"><span> casually</span></span>
+<span class="line"><span> secretly</span></span>
+<span class="line"><span> assess</span></span>
+<span class="line"><span> form</span></span>
+<span class="line"><span> prove</span></span>
+<span class="line"><span> slight</span></span>
+<span class="line"><span> curiosity</span></span>
+<span class="line"><span> disappointment</span></span>
+<span class="line"><span> persuade</span></span>
+<span class="line"><span> refuse</span></span>
+<span class="line"><span> secure</span></span>
+<span class="line"><span> repeat</span></span>
+<span class="line"><span> recite</span></span>
+<span class="line"><span> duty</span></span>
+<span class="line"><span> greet</span></span></code></pre><div class="line-numbers" aria-hidden="true" style="counter-reset:line-number 0;"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="sentences" tabindex="-1"><a class="header-anchor" href="#sentences"><span>Sentences</span></a></h2><h3 id="活动类作文" tabindex="-1"><a class="header-anchor" href="#活动类作文"><span>活动类作文</span></a></h3><ul><li><p>From this experience, not only have I learnt that knowledge comes alive when put into practice, but it has also contributed to my personal growth and success.</p></li><li><p>All in all, in my English learning proess, I firmly belive that &quot;Where there is a will, there is a way&quot;. The man who has made up his mind to win never say impossible. And learning English is the key to unlocking the treasures of knowledge.</p></li><li><p>As the saying goes, a journey of a thousand miles begins with a single step. With motivation and determination, anyone can succeed in learning English.</p></li><li><p>From the moment I started the course, I fell in love with it. Not only did it provide me with a chance to experience and appreciate the beauty of calligraphy, but it also served as a window for me to explore the rich traditional Chinese culture.</p></li></ul>`,5)]))}const c=s(l,[["render",t],["__file","1.html.vue"]]),d=JSON.parse('{"path":"/learn/%E8%8B%B1%E8%AF%AD/CET-4/1.html","title":"2024/10/9","lang":"zh-CN","frontmatter":{"tags":["学习"],"title":"2024/10/9","prev":{"link":"./0.md","text":"四级试题"},"next":{"link":"./2.md","text":"CET-4_作文"},"description":"Words Sentences 活动类作文 From this experience, not only have I learnt that knowledge comes alive when put into practice, but it has also contributed to my personal growth and succe...","head":[["meta",{"property":"og:url","content":"https://docs.wenturc.com/learn/%E8%8B%B1%E8%AF%AD/CET-4/1.html"}],["meta",{"property":"og:site_name","content":"WentUrc Docs"}],["meta",{"property":"og:title","content":"2024/10/9"}],["meta",{"property":"og:description","content":"Words Sentences 活动类作文 From this experience, not only have I learnt that knowledge comes alive when put into practice, but it has also contributed to my personal growth and succe..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-11-28T14:42:55.000Z"}],["meta",{"property":"article:tag","content":"学习"}],["meta",{"property":"article:modified_time","content":"2024-11-28T14:42:55.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"2024/10/9\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-11-28T14:42:55.000Z\\",\\"author\\":[]}"]]},"headers":[{"level":2,"title":"Words","slug":"words","link":"#words","children":[]},{"level":2,"title":"Sentences","slug":"sentences","link":"#sentences","children":[{"level":3,"title":"活动类作文","slug":"活动类作文","link":"#活动类作文","children":[]}]}],"readingTime":{"minutes":0.73,"words":219},"git":{"createdTime":1729338822000,"updatedTime":1732804975000,"contributors":[{"name":"冰苷晶","email":"2749332490@qq.com","commits":4,"url":"https://github.com/冰苷晶"}]},"autoDesc":true,"filePathRelative":"learn/英语/CET-4/1.md","categoryList":[{"id":"339d8d","sort":10000,"name":"learn"},{"id":"c44fc1","sort":10001,"name":"英语"},{"id":"6a9995","sort":10003,"name":"CET-4"}],"bulletin":false}');export{c as comp,d as data};
